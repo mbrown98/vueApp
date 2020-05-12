1778faf1356b13abdde90b3b2bfe5cf57b619468
Matthew Brown <matthewbrown0798@gmail.com>
2020-05-11 20:05:38 -0400

    added stock api routes

--- src/components/CompanyNews.vue
+++ src/components/CompanyNews.vue
@@ -2,9 +2,9 @@
   <div id="news">
     <h1>News {{ stock }}</h1>
 
-    <div v-for="data in newsData" :key="data.id">
+    <!-- <div v-for="data in newsData" :key="data.id">
       <a href="`${data.url}`">{{ data.headline }}</a>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -22,6 +22,9 @@ export default {
     return {
       name: "",
       newsData: {},
+      peers: [],
+      majorDevelopments: [],
+      recTrends: [],
     };
   },
   computed: {
@@ -31,15 +34,20 @@ export default {
   },
   watch: {
     stock() {
-      this.updateNews();
+      //   this.getCompanyNews();
+      //   this.getPeers();
+      //   this.getMajorDevelopments();
+      this.getNewsSentiment();
+      this.getRecommendationTrends();
     },
   },
   created() {
-    this.updateNews();
+    this.getCompanyNews();
+    this.getPeers();
   },
 
   methods: {
-    updateNews: function() {
+    getCompanyNews: function() {
       console.log("mews");
       axios
         .get(
@@ -49,6 +57,67 @@ export default {
           this.newsData = response.data;
         })
         .catch((err) => {
+          console.log(err);
+        });
+    },
+    getPeers: function() {
+      axios
+        .get(
+          `https://finnhub.io/api/v1/stock/peers?symbol=${this.stock}&token=bqsr1bfrh5rc3vfduat0`
+        )
+        .then((response) => {
+          console.log("peer", response);
+          this.peers = response.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getMajorDevelopments() {
+      axios
+        .get(
+          `https://finnhub.io/api/v1/major-development?symbol=${this.stock}&token=bqsr1bfrh5rc3vfduat0`
+        )
+        .then((response) => {
+          console.log("major", response.data.majorDevelopment);
+          this.majorDevelopments = response.data.majorDevelopment;
+        })
+        .then.catch((err) => {
+          console.log(err);
+        });
+    },
+    getNewsSentiment() {
+      axios
+        .get(
+          `https://finnhub.io/api/v1/news-sentiment?symbol=${this.stock}&token=bqsr1bfrh5rc3vfduat0`
+        )
+        .then((response) => {
+          console.log(response);
+          this.newsSentiment = response.data;
+        })
+        .then.catch((err) => {
+          console.log(err);
+        });
+    },
+    getRecommendationTrends() {
+      axios
+        .get(
+          `https://finnhub.io/api/v1/stock/recommendation?symbol=${this.stock}&token=bqsr1bfrh5rc3vfduat0`
+        )
+        .then((response) => {
+          this.recTrends = response.data;
+        })
+        .then.catch((err) => {
+          console.log(err);
+        });
+    },
+    getHistoricalPrices() {
+      axios
+        .get(``)
+        .then((response) => {
+          this.newsData = response.data;
+        })
+        .then.catch((err) => {
           console.log(err);
         });
     },
